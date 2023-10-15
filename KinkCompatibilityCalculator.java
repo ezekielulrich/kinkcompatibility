0457de2a6b3184d14069c8e5ccb38ad4418be972
Ezekiel Ulrich <ezekielulrich@gmail.com>
2023-10-15 12:45:21 -0400

    Made it look a little better

--- KinkCompatibilityCalculator.java
+++ KinkCompatibilityCalculator.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class KinkCompatibilityCalculator {
     private static final HashMap<String, String> pairs = new HashMap<>();
     private static int num = 0;
-
-
     static {
         pairs.put("Rigger", "Rope bunny");
         pairs.put("Sadist", "Masochist");
@@ -77,18 +77,42 @@ public class KinkCompatibilityCalculator {
 
         ArrayList<HashMap<String, Double>> playmates = new ArrayList<>();
 
-        // Main frame
+        // Main frame and panel
         JFrame frame = new JFrame("Kink Compatibility Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(300, 500);
 
         // Area for entering results
         JTextArea textArea = new JTextArea();
         textArea.setPreferredSize(new Dimension(150, 50));
         textArea.setMargin(new Insets(10, 20, 10, 20));
+        textArea.setOpaque(false);
+        textArea.setForeground(Color.WHITE);
+        textArea.setText("Enter copied results here."); // Set initial text
+
+        textArea.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                // When the field gains focus, set the text color to be fully opaque
+                textArea.setForeground(Color.WHITE);
+                if (textArea.getText().equals("Enter copied results here.")) {
+                    textArea.setText(""); // Clear initial text
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                // When the field loses focus, restore the transparent text color if it's empty
+                if (textArea.getText().isEmpty()) {
+                    textArea.setForeground(Color.WHITE);
+                    textArea.setText("Enter copied results here.");
+                }
+            }
+        });
 
         // Button for saving results
         JButton submitButton = new JButton("Add Results");
+        submitButton.setBackground(Color.BLACK);
 
         submitButton.addActionListener(new ActionListener() {
             @Override
@@ -103,9 +127,9 @@ public class KinkCompatibilityCalculator {
             }
         });
 
-
         // Calculate button
         JButton calculateButton = new JButton("Calculate Compatibility");
+        calculateButton.setBackground(Color.BLACK);
 
         calculateButton.addActionListener(new ActionListener() {
             @Override
@@ -119,14 +143,15 @@ public class KinkCompatibilityCalculator {
             }
         });
 
-
         // Define layout
         Container contentPane = frame.getContentPane();
+        contentPane.setBackground(Color.BLACK); // Set background color of the panel
+
+        // Add elements
         contentPane.setLayout(new BorderLayout());
         contentPane.add(textArea, BorderLayout.CENTER);
         contentPane.add(calculateButton, BorderLayout.SOUTH);
         contentPane.add(submitButton, BorderLayout.NORTH);
-
 
         frame.setVisible(true);
     }
